e8ac44c8825045bc824035b00b9e40f86b78214e
Rob Blackbourn <rob.blackbourn@googlemail.com>
2019-03-29 07:35:09 +0000

    Version updated from 0.2.0 to 0.3.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 setup(
     name='bareclient',
