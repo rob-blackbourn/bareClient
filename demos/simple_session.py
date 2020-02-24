20ab4100b7ad83e28818eb7c85ee9c6cb5f6e0e9
Rob Blackbourn <rblackbourn@bhdgsystematic.com>
2020-02-24 09:15:01 +0000

    Improved the session example

--- demos/simple_session.py
+++ demos/simple_session.py
@@ -2,38 +2,44 @@
 
 import asyncio
 import json
-import logging
 
 from bareutils import text_reader
+import bareutils.header as header
 import bareutils.response_code as response_code
 from bareclient import HttpSession
-
-logging.basicConfig(level=logging.DEBUG)
-
-# https://jsonplaceholder.typicode.com/users/1/posts
-# https://jsonplaceholder.typicode.com/posts/1/comments?postId=1
 
 
 async def main() -> None:
     session = HttpSession('https://jsonplaceholder.typicode.com')
     async with session.request('/users/1/posts', method='GET') as response:
-        print(response)
-        print(session.cookies)
+        # We expect a session cookie to be sent on the initial request.
+        set_cookie = header.find(b'set-cookie', response['headers'])
+        if set_cookie:
+            print('We received a session cookie: ' + set_cookie.decode())
+        else:
+            print("No session cookie")
         if not response_code.is_successful(response['status_code']):
             print("failed")
             return
         text = await text_reader(response['body'])
         posts = json.loads(text)
-        print(posts)
+        print(f'We received {len(posts)} posts')
         for post in posts:
             path = f'/posts/{post["id"]}/comments'
+            print(f'Requesting comments from "{path}""')
             async with session.request(path, method='GET') as response:
+                # As we are sent the session cookie we do not expect to receive another one.
+                set_cookie = header.find(b'set-cookie', response['headers'])
+                if set_cookie:
+                    print('We received a session cookie: ' + set_cookie.decode())
+                else:
+                    print("No session cookie")
                 if not response_code.is_successful(response['status_code']):
                     print("failed")
                     return
                 text = await text_reader(response['body'])
                 comments = json.loads(text)
-                print(comments)
+                print(f'We received {len(comments)} comments')
 
 
 asyncio.run(main())
