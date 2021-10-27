966898015abf61bdaff58340b5fd4acfa19ae7c1
Rob Blackbourn <rob.blackbourn@googlemail.com>
2021-10-27 06:57:05 +0100

    Used platform.uname

--- bareclient/constants.py
+++ bareclient/constants.py
@@ -1,13 +1,13 @@
 """Constants"""
 
-import os
+import platform
 
 import pkg_resources
 
 DIST_VERSION = pkg_resources.get_distribution('bareclient').version
 
 
-SYSNAME, HOST, RELEASE, OS_VERSION, MACHINE = os.uname()
+SYSNAME, HOST, RELEASE, OS_VERSION, MACHINE, PROCESSOR = platform.uname()
 
 
 USER_AGENT = f'bareClient/{DIST_VERSION} ({SYSNAME}; {RELEASE}; {MACHINE})'.encode(
